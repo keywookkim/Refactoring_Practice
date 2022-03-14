7c263bd1b9ad3ea66c1195cd21642f64d432077f
김기욱 <1218kim23@gmail.com>
2022-03-14 12:55:36 +0900

    refactor: [chapter1] 다형성 활용 - 장르별로 서브클래스 생성 - amountFor, volumeCreditsFor 조건부로직을 생성함수 하나로 이동

--- Chapter1/Polymorphoism/createStatementData.js
+++ Chapter1/Polymorphoism/createStatementData.js
@@ -8,7 +8,8 @@ exports.createStatementData = function (invoice, plays) { //필요 데이터 추
   return result;
 
   function enrichPerformances(aPerformance) {
-    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+    const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
+
     const result = Object.assign({}, aPerformance); //얕은 복사 수행
     result.play = calculator.play; // 중간데이터에 연극정보를 저장
     result.amount = calculator.amount; // 중간데이터에 수량계산한 중간결과값을 저장
@@ -39,42 +40,50 @@ exports.createStatementData = function (invoice, plays) { //필요 데이터 추
   }
 }
 
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`)
+  }
+}
 class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
     this.performances = aPerformance;
     this.play = aPlay;
   }
 
-  get amount() { // amountFor 함수 코드를 계산기 클래스로 복사
-    let result = 0; 
-    switch(this.play.type) { // 매개변수로 받던 정보를 계산기 필드에서 바로 얻음
-      case "tragedy": // 비극
-        result = 40000;
-        if (this.performances.audience > 30) {
-          result += 1000 * (this.performances.audience - 30);
-        }
-        break;
-      case "comedy": // 희극
-        result = 30000;
-        if (this.performances.audience > 20) {
-          result += 10000 + 500 * (this.performances.audience - 20)
-        } 
-        result += 300 * this.performances.audience;
-        break;
-      default: 
-        throw new Error(`알 수 없는 장르: ${this.play.type}`)
-    }
-    return result    
+  get amount() {
+    throw new Error('서브클래스에서 처리하도록 설계되었습니다.')  
   }
 
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performances.audience - 30, 0);
-    // 희극 관객 5명마다 추가포인트를 제공한다
-    if ("comedy" == this.play.type) {
-      result += Math.floor(this.performances.audience / 5);
-    }
-    return result;
+    return Math.max(this.performances.audience - 30, 0);
   }
 }
 
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() { 
+    let result = 40000;
+    if (this.performances.audience > 30) {
+      result += 1000 * (this.performances.audience - 30);
+    }
+    return result    
+  }    
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() { 
+    let result = 30000;
+    if (this.performances.audience > 20) {
+        result += 10000 + 500 * (this.performances.audience - 20)
+    } 
+    result += 300 * this.performances.audience;
+    return result    
+  }  
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performances.audience / 5);
+  }
+}
